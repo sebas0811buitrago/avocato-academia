510c2c37ac3f59d19784c4206122757500e3c324
sebas0811buitrago <86497666+sebas0811buitrago@users.noreply.github.com>
2024-10-14 18:52:17 -0500

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,13 @@
-
-import eslintConfigPrettier from "eslint-config-prettier";
-import astro from "eslint-plugin-astro";
-
+import pluginJs from "@eslint/js"
+import eslintPluginAstro from "eslint-plugin-astro"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 export default [
-  {
-    files: ["**/*.{astro,js,mjs,cjs,ts}"],
-
-    rules: {
-
-      ...astro.configs.recommended.rules,
-       ...astro.configs["flat/jsx-a11y-recommended"].rules, // Adds accessibility rules
-    },
-  }, 
-  eslintConfigPrettier
-];
-
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginAstro.configs["flat/jsx-a11y-recommended"],
+	...eslintPluginAstro.configs.recommended,
+]
